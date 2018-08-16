ee807f85d979798dccb07b7883358272e2a5d7e1
Volodymyr Danyliv <vdanyliv@lohika.com>
2018-08-16 10:42:33 +0300

    Recirsive method changed to loops and Board assigned to method

--- Test/core/Task_new.py
+++ Test/core/Task_new.py
@@ -15,23 +15,21 @@ def get_player():
         print 'Player 2, your sign is %s: ' % current_player
 
 
-def get_field():
-    try:
-        row = int(raw_input("Horisontal Row: ")) - 1
-        column = int(raw_input("Vertical Col: ")) - 1
-        if row in range(0, 3) and column in range(0, 3):
-            if board[row][column] == default_sign:
-                field = board[row][column] = current_player
-                return field
+def get_field(board):
+    while True:
+        try:
+            row = int(raw_input("Horisontal Row: ")) - 1
+            column = int(raw_input("Vertical Col: ")) - 1
+            if row in range(0, 3) and column in range(0, 3):
+                while board[row][column] == default_sign:
+                        field = board[row][column] = current_player
+                        return field
+                else:
+                    print "Incorrect choice, please try again..."
             else:
-                print "Incorrect choice, please try again..."
-                get_field()
-        else:
-            print "There is no such row or column, please try again"
-            get_field()
-    except(ValueError, NameError, KeyboardInterrupt, UnboundLocalError, TypeError):
-        print "You can not input characters or white space"
-        get_field()
+                print "There is no such row or column, please try again"
+        except(ValueError, NameError, KeyboardInterrupt, UnboundLocalError, TypeError):
+            print "You can not input characters or white space"
 
 
 def print_board():
@@ -41,7 +39,8 @@ def print_board():
 
 for current_player in itertools.cycle(sign):
     get_player()
-    selected_field = get_field()
+    selected_field = get_field(board)
     print_board()
+
 
 
