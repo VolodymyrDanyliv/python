da0590ec340878b4571cbc19ec8ff97ba3cb2ec6
Volodymyr Danyliv <vdanyliv@lohika.com>
2018-08-28 12:36:46 +0300

    Get fact task

---
+++ Test/core/Fact_task.py
@@ -0,0 +1,34 @@
+import urllib2
+import json
+
+while True:
+    URL = 'https://the-cat-fact.herokuapp.com/api/randomfact'
+    status_code = 200
+
+    answer = raw_input("Do you want to know something interesting?: ")
+    if answer.lower() == 'yes':
+        try:
+            '''Get URL'''
+            response = urllib2.urlopen(URL)
+            '''Get Status Code'''
+            actual_code = response.getcode()
+            if actual_code == status_code:
+                '''Get Dictionary JSON'''
+                values_string = json.loads(response.read())
+                '''Access to FACT value'''
+                fact = values_string.get('data')[0].get('fact')
+                print fact
+            else:
+                print "Recourse unavailable"
+        except urllib2.HTTPError:
+            choice = raw_input("Connection error occurred. Do you want to continue?: ")
+            if choice.lower() == 'yes':
+                print ""
+            elif choice.lower() == 'no':
+                exit(0)
+            else:
+                print "Please type Yes or No"
+    elif answer.lower() == 'no':
+        exit(0)
+    else:
+        print "Please type Yes or No"
